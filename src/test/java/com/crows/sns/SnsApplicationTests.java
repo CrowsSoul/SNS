2c2b979648bd15778e2d97d528be92333050d120
Crows <yddonald@163.com>
2024-07-26 16:13:04 +0800

    项目初步创建

---
+++ src/test/java/com/crows/sns/SnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crows.sns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
