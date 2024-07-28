1103066e7e108620ce09d80e07f09681fe03ad18
Crows <yddonald@163.com>
2024-07-28 08:27:36 +0800

    获取所有的评论 完成

---
+++ src/main/java/com/crows/sns/mapper/CommentMapper.java
@@ -0,0 +1,19 @@
+package com.crows.sns.mapper;
+
+import com.crows.sns.pojo.Comment;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
+public interface CommentMapper {
+    @Select("SELECT * FROM comments")
+    @Results({
+            @Result(property = "id", column = "comment_id"),
+            @Result(property = "blogId", column = "post_id")
+    })
+    public List<Comment> getAllComments();
+}
