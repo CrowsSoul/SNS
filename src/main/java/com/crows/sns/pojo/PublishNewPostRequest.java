78b1970d8baba084ea3803a881132ebe8c397cd4
Crows <yddonald@163.com>
2024-07-30 08:57:05 +0800

    所有功能全部完成，修改了精选的bug

--- src/main/java/com/crows/sns/pojo/PublishNewPostRequest.java
+++ src/main/java/com/crows/sns/pojo/PublishNewPostRequest.java
@@ -1,5 +1,6 @@
 package com.crows.sns.pojo;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,5 +13,6 @@ public class PublishNewPostRequest {
     private String content;
     private String author;
     private String status;
+    @JsonProperty("isRecommended")
     private boolean isRecommended;
 }
